6d483a4abb4ba4466300e911f030920f0150af92
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-02 00:06:42 +0400

    update SEO

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,41 @@ import { Testimonials } from "./components/Testimonials/Testimonials";
 import Title from "./components/Title/Title";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import { Helmet } from "react-helmet-async";
 
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <title>Digital Air Condition - Top AC Servicesr</title>
+        <meta
+          name="description"
+          content="Comprehensive Cooling and Air Conditioning Solutions in Abu Dhabi - Digital Air Condition"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Additional meta tags */}
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Digital Air Condition" />
+        <meta charset="UTF-8" />
+        <meta name="language" content="en-US" />
+        <meta name="language" content="ar-SA" />
+
+        {/* Social Media Optimization */}
+        <meta
+          property="og:title"
+          content="Top AC Services | Digital Air Condition (Abu Dhabi)"
+        />
+        <meta
+          property="og:description"
+          content="Cool any space with Digital Air Condition expert AC solutions. Contact us today!"
+        />
+
+        {/* <meta
+          property="og:image"
+          content="https://yourwebsite.com/social-media.png"
+        /> */}
+      </Helmet>
       <Navbar />
       <Hero />
       <Title
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,26 +3,28 @@ import arrow_icon from "../../assets/arrow.png";
 import { Link } from "react-scroll";
 const Hero = () => {
   return (
-    <div className="hero container">
-      <div className="hero-text">
-        <h1>DIGITAL AIR CONDITION</h1>
-        <h3>MAINTENANCE OF AC</h3>
-        <p>
-          Enjoy comfort and refreshment with one of the best air conditioning
-          companies in the United Arab Emirates
-        </p>
+    <>
+      <div className="hero container">
+        <div className="hero-text">
+          <h1>DIGITAL AIR CONDITION</h1>
+          <h3>MAINTENANCE OF AC</h3>
+          <p>
+            Enjoy comfort and refreshment with one of the best air conditioning
+            companies in the United Arab Emirates
+          </p>
 
-        <Link
-          className="btn"
-          to="services"
-          smooth={true}
-          offset={-280}
-          duration={500}
-        >
-          Explore more <img src={arrow_icon} className="arrow" />
-        </Link>
+          <Link
+            className="btn"
+            to="services"
+            smooth={true}
+            offset={-280}
+            duration={500}
+          >
+            Explore more <img src={arrow_icon} className="arrow" />
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
